4a7b056a5e9196830112dce88d324a60c72b360c
SuperToad <super-toad@hotmail.fr>
2015-11-16 19:31:14 +0100

    Second sequential version
    Works but with a margin of error.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include <cmath>
 using namespace std;
 
-int f(float x, float tableau[], int degre)
+int f(double x, double tableau[], int degre)
 {
 	int puissance = degre;
-	float resultat = 0;
-	for (int i=0; i<degre; i++)
+	double resultat = 0;
+	for (int i=0; i<degre+1; i++)
 	{
-		resultat += pow(tableau[i], puissance);
+		resultat += pow(x, puissance)*tableau[i];
 		puissance -= 1;
 	}
 	return resultat;
@@ -16,40 +16,48 @@ int f(float x, float tableau[], int degre)
 
 int main()
 {
-	
+
 	int degre;
 	cout << "Quel est le degre du polynome? : \n";
 	cin >> degre;
-	float tableau[degre+1];
-	
-	
-	float valeur;
+	double tableau[degre+1];
+
+
+	double valeur;
 	cout << "Entrez les differentes valeurs : \n";
-	
+
 	for(int i=0; i<degre+1; i++)
 	{
 		cin >> valeur;
 		cout << "\n";
 		tableau[i] = valeur;
 	}
-	
+
 	int borne1, borne2;
 	cout << "Entrez les deux bornes : \n";
 	cin >> borne1;
 	cout << "\n";
 	cin >> borne2;
-	
-	const float DIVISIONS = (borne1 - borne2)/3;
-	
-	float aire = 0;
-	for(float i=borne1; i<borne2; i+=DIVISIONS)
+
+	const double DIVISIONS = 300000000;
+
+	double aire = 0;
+
+	/*for(double i=borne1; i<borne2; i+=DIVISIONS)
 	{
 		aire += (DIVISIONS)*((f(i, tableau, degre)+f(borne1+DIVISIONS, tableau, degre))/2);
 		cout << i << "\n";
+	}*/
+
+	double sigma = 0;
+	for(int i=1; i<DIVISIONS-1;i++)
+	{
+        sigma += f(((double)borne1+(double)i*((double)(borne2-borne1)/DIVISIONS)), tableau, degre);
 	}
-	
-	cout << aire << "\n"	;
-	
+
+	aire = ((double)(borne2 - borne1) / DIVISIONS) * (((f(borne1, tableau, degre) + f(borne2, tableau, degre))/2) + sigma);
+	cout << aire;
+
 	return 0;
 }
 
