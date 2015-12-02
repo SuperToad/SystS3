09963f90407252c313e12d37f5bab1357a9b90aa
SuperToad <super-toad@hotmail.fr>
2015-12-02 16:01:26 +0100

    Timed version
    This version gives the execution time.

--- Threads.cpp
+++ Threads.cpp
@@ -1,28 +1,9 @@
 #include <iostream>
 #include <cmath>
-#include <pthread.h>
+#include <ctime>
+#include <sys/time.h>
 using namespace std;
-//compilation : g++ -Wall threads.cpp -o threads -lpthread
-
-/*****************************************************PARAMETRES*****************************************************/
-	// Degre du polynome
-	int degre = 5;
-	// Valeurs de chaque indice dans un tableau de taille degre+1
-	double tableau[] = {5, 6, 18, 12, 3, 1};
-
-	// Bornes min et max
-	double borne1 = 0.0;
-	double borne2 = 5.0;
-
-	// Nombre de divisions qui seront effectuees
-	// Définit la précision
-	const double DIVISIONS = 3000000;
-	
-	// Nombre de threads crées
-	const int nbThreads = 10;
-/********************************************************************************************************************/
-double pas = ((double)(borne2 - borne1) / DIVISIONS);
-int incr = 0;
+//compilation : g++ -Wall sequential.cpp -o sequential
 
 // Retourne la valeur de x
 double f(double x, double tableau[], int degre)
@@ -37,58 +18,44 @@ double f(double x, double tableau[], int degre)
 	return resultat;
 }
 
-// Calcul du sigma
-// La fonction que les threads utilisent
-void * sigma (void * valeur)
-{
-	double * val = (double * ) valeur;
-	double * tempSigma = new double();
-	*tempSigma = 0;
-
-	cerr << 1+((DIVISIONS/nbThreads)*(*val)) << " "<< (DIVISIONS/nbThreads)*((*val)+1) << "\n";
-
-	for(int i=1+((DIVISIONS/nbThreads)*(*val)); i< (DIVISIONS/nbThreads)*((*val)+1);i++)
-	{
-		(*tempSigma) += f((borne1+i*pas), tableau, degre);
-		incr++;
-	}
-	cerr << "Fin thread " << "\n";
-	return (tempSigma);
-}
-
 int main()
 {
-	pthread_t thtab[nbThreads];
+	struct timeval begin, end;
+	gettimeofday(&begin, NULL);
+/*****************************************************PARAMETRES*****************************************************/
+	// Degre du polynome
+	int degre = 5;
+	// Valeurs de chaque indice dans un tableau de taille degre+1
+	double tableau[] = {5, 6, 18, 12, 3, 1};
+
+	// Bornes min et max
+	double borne1 = 0.0;
+	double borne2 = 5.0;
+
+	// Nombre de divisions qui seront effectuees
+	// Définit la précision
+	const int DIVISIONS = 300000;
+/********************************************************************************************************************/
+
+	double pas = ((double)(borne2 - borne1) / DIVISIONS);
 
 	double aire = 0;
-	double totSigma = 0;
+	double sigma = 0;
 
-	double * t = new double[nbThreads];
-	
-	for (unsigned int i=0; i<nbThreads; ++i)
-		t[i] = i;
-	
-	for (unsigned int th=0; th<nbThreads; th++)
+	// Calcul de sigma
+	for(int i=1; i<DIVISIONS;i++)
 	{
-		pthread_create(&thtab[th], NULL, sigma, (void*) &t[th]);
-		cerr << "lancement du thread " << th << "\n";
+        	sigma += f((borne1+i*pas), tableau, degre);
 	}
 
-	cerr << "Tous les threads ont été lancés\n";
-
-	void * r;	
-	double * res;
-	for (unsigned int th=0; th<nbThreads; ++th) {
-			pthread_join(thtab[th], &r);
-			res = (double*) r;
-			
-			totSigma += (*res);
-			cerr << "arrivée du thread " << th << " r= " << *res << " result+ = " << totSigma << "\n"; 
-	}
-	cerr << incr << endl;
 	// Calcul de l'intégrale
-	aire = pas * (((f(borne1, tableau, degre) + f(borne2, tableau, degre))/2) + totSigma);
-	cout << aire << endl;
-
+	aire = pas * (((f(borne1, tableau, degre) + f(borne2, tableau, degre))/2) + sigma);
+	cout << "L'aire vaut : " << aire << endl;
+	
+	gettimeofday(&end, NULL);
+	double totalTime = ((double)(1000*(end.tv_sec-begin.tv_sec)+((end.tv_usec-begin.tv_usec)/1000)));
+	//difftime(end, begin);
+	cout << "Temps d'execution : " << totalTime << endl;
+	
 	return 0;
 }
